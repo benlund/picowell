75323d9cf19b8961a7b1c1a0b09af5c64077edd2
Ben Lund <ben@benlund.com>
2022-08-13 09:47:09 -0700

    Sensor: refactor to be linearly calbrated with two readings, no PSI intermediary calc

--- lib/sensor.py
+++ lib/sensor.py
@@ -1,20 +1,21 @@
 import machine
 import time
 
-FeetPerPsi = const(2.31)
 MaxAdc = const(65535)
 MaxVolts = const(3.3)
-MaxPSI = const(21.0) # This is not measured, but calculated as the value at 3.3V based on sensor specification
 
 class Sensor:
 
-    def __init__(self, adc_gpio_pin_num, zero_adc_reading):
+    def __init__(self, adc_gpio_pin_num, low_adc_anchor, high_adc_anchor):
         print('Sensor#init')
         print('  adc_gpio_pin_num = ', adc_gpio_pin_num)
-        print('  zero_adc_reading = ', zero_adc_reading)
+        print('  low_adc_anchor = ', low_adc_anchor)
+        print('  high_adc_anchor = ', high_adc_anchor)
 
         self.pin = machine.ADC(adc_gpio_pin_num)
-        self.zero_psi_volts = self.calc_volts(zero_adc_reading)
+        self.low_adc = low_adc_anchor['adc']
+        self.low_feet = low_adc_anchor['feet']
+        self.feet_per_adc = (high_adc_anchor['feet'] - low_adc_anchor['feet']) / (high_adc_anchor['adc'] - low_adc_anchor['adc'])
 
 
     def reading(self, num_readings_to_take=5, reading_wait_s=1):
@@ -28,15 +29,13 @@ class Sensor:
 
         ave_adc = readings_total / num_readings_to_take
         volts = self.calc_volts(ave_adc)
-        psi = self.calc_psi(volts)
-        feet = self.calc_feet(psi)
+        feet = self.calc_feet(ave_adc)
 
         return {
             'adc': ave_adc,
             'num': num_readings_to_take,
             'wait': reading_wait_s,
             'volts': volts,
-            'psi': psi,
             'feet': feet
         }
 
@@ -49,9 +48,5 @@ class Sensor:
         return adc * MaxVolts / MaxAdc
 
 
-    def calc_psi(self, volts):
-        return (volts - self.zero_psi_volts) * MaxPSI / (MaxVolts - self.zero_psi_volts)
-
-
-    def calc_feet(self, psi):
-        return FeetPerPsi * psi
+    def calc_feet(self, adc):
+        return self.low_feet + ( (adc - self.low_adc) * self.feet_per_adc )
