fc1cbaa2cf29f7009b527dc59cf4c1d58e4b489c
Ben Lund <ben@benlund.com>
2022-08-22 17:12:52 -0700

    Sensor: fix to support rounding / dropping bits

--- lib/sensor.py
+++ lib/sensor.py
@@ -1,7 +1,7 @@
 import machine
 import time
 
-MaxAdc = const(65535)
+MaxAdc = const(2 ** 16)
 MaxVolts = const(3.3)
 
 class Sensor:
@@ -18,23 +18,28 @@ class Sensor:
         self.feet_per_adc = (high_adc_anchor['feet'] - low_adc_anchor['feet']) / (high_adc_anchor['adc'] - low_adc_anchor['adc'])
 
 
-    def reading(self, num_readings_to_take=5, reading_wait_s=1):
+    def reading(self, num_readings_to_take=8, reading_wait_s=1, num_bits_to_ignore=0):
         readings_taken = 0
         readings_total = 0.0
         while readings_taken < num_readings_to_take:
             if readings_taken > 0:
                 time.sleep(reading_wait_s)
-            readings_total += self.do_reading()
+            readings_total += (self.do_reading() >> num_bits_to_ignore << num_bits_to_ignore)
             readings_taken += 1
 
-        ave_adc = readings_total / num_readings_to_take
+        ave_adc = int(readings_total / num_readings_to_take) >> num_bits_to_ignore << num_bits_to_ignore
         volts = self.calc_volts(ave_adc)
         feet = self.calc_feet(ave_adc)
+        if num_bits_to_ignore > 0:
+            ##@@ TODO calc rounding factor based on bits_to_ignore
+            volts = round(volts, 3)
+            feet = round(feet, 3)
 
         return {
             'adc': ave_adc,
             'num': num_readings_to_take,
             'wait': reading_wait_s,
+            'bits_ignored': num_bits_to_ignore,
             'volts': volts,
             'feet': feet
         }
